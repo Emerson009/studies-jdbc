39ec71e8d070a8b1ea635a7990a49fad5fda27a2
EmersonH01 <henrique.emerson@cruzvita.com>
2022-12-12 11:27:39 -0300

    primeira-conexao

--- loja-virtual/src/TestaConexao.java
+++ loja-virtual/src/TestaConexao.java
@@ -1,3 +1,13 @@
-public class TestaConexao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class TestaConexao {
+    public static void main(String[] args) throws SQLException{
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
+                        "root", "EticumSans17@#$");
+
+            connection.close();
+        }
 }
