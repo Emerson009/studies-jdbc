9335ed3741f1114b7fad45355c4336fdd45c70ed
Emerson009 <henriqueemerson023@gmail.com>
2022-12-13 11:29:10 -0300

    removendo-autocommit

--- loja-virtual/src/PreparedStatement/TestaInsercaoComParametro.java
+++ loja-virtual/src/PreparedStatement/TestaInsercaoComParametro.java
@@ -5,17 +5,23 @@ import java.sql.*;
 public class TestaInsercaoComParametro {
 
     public static void main(String[] args) throws SQLException {
-        String nome = "Geladeira";
-        String descricao = "Geladeira com filtro";
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.recuperarConexao();
+        connection.setAutoCommit(false);
 
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
                 Statement.RETURN_GENERATED_KEYS);
 
+        adicionarVariavel("LG Smart TV", "50 polegadas", preparedStatement);
+        adicionarVariavel("Radio", "Radio de bateria", preparedStatement);
+    }
+
+    public static void adicionarVariavel(String nome, String descricao, PreparedStatement preparedStatement) throws SQLException {
         preparedStatement.setString(1, nome);
         preparedStatement.setString(2, descricao);
+
+        
 
         preparedStatement.execute();
 
@@ -24,5 +30,6 @@ public class TestaInsercaoComParametro {
             Integer id = resultSet.getInt(1);
             System.out.println("O id criado foi: " + id);
         }
+        resultSet.close();
     }
 }
