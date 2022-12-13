4518088f0ac5eb11841f35081840d3920aeb496f
Emerson009 <henriqueemerson023@gmail.com>
2022-12-13 11:42:57 -0300

    refatorando-commit

--- loja-virtual/src/PreparedStatement/TestaInsercaoComParametro.java
+++ loja-virtual/src/PreparedStatement/TestaInsercaoComParametro.java
@@ -7,21 +7,35 @@ public class TestaInsercaoComParametro {
     public static void main(String[] args) throws SQLException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.recuperarConexao();
+
         connection.setAutoCommit(false);
 
+        try{
         PreparedStatement preparedStatement = connection.prepareStatement(
                 "INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
                 Statement.RETURN_GENERATED_KEYS);
 
         adicionarVariavel("LG Smart TV", "50 polegadas", preparedStatement);
         adicionarVariavel("Radio", "Radio de bateria", preparedStatement);
+
+        connection.commit();
+
+        preparedStatement.close();
+        connection.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("ROLLBACK EXECUTADO");
+            connection.rollback();
+        }
     }
 
     public static void adicionarVariavel(String nome, String descricao, PreparedStatement preparedStatement) throws SQLException {
         preparedStatement.setString(1, nome);
         preparedStatement.setString(2, descricao);
 
-        
+        if(nome.equals("Radio")){
+            throw new RuntimeException("Não foi possível adicionar o produto");
+        }
 
         preparedStatement.execute();
 
